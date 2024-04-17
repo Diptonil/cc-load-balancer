5c43e7a26990eeff77161503262063e2f93f19d9
Thoroughfare by the Brooks <diptonilr@gmail.com>
2024-04-17 14:40:17 +0530

    Server set up

--- cclb.py
+++ cclb.py
@@ -1,25 +1,22 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-
-HOST = "localhost"
-PORT = 80
+from utils.constants import HOST, PORT
+from utils.loggers import app_logger
 
 
 class Server(BaseHTTPRequestHandler):
+    def log_message(self) -> None:
+        app_logger.info(f'Recieved request from {self.client_address[0]}: "{self.command} {self.path} {self.request_version}".')
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
+        self.send_header('Cache-Control', 'no-cache')
         self.end_headers()
-        
-        # Send message back to client
-        message = "Hello, world!"
-        
-        # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
-        return
 
 
 if __name__ == "__main__":
     httpd = HTTPServer((HOST, PORT), Server)
+    app_logger.info("Load balancer started.")
     httpd.serve_forever()
     
